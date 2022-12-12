495cb3159a49b69132f3332e0e5971ddec69b106
Roman <rallyshops@gmail.com>
2022-12-12 21:47:55 +0200

    generate pdf

--- backend-cafe-nodejs/routes/bill.js
+++ backend-cafe-nodejs/routes/bill.js
@@ -5,7 +5,7 @@ let ejs = require('ejs');
 let pdf = require('html-pdf');
 let path = require('path');
 let fs = require('fs');
-let uuid = require('uuis');
+let uuid = require('uuid');
 let auth = require('../services/authentication');
 
 router.post('/generateReport', auth.authenticateToken, (req, res) => {
@@ -22,10 +22,10 @@ router.post('/generateReport', auth.authenticateToken, (req, res) => {
     orderDetails.paymentMethod, 
     orderDetails.totalAmount, 
     orderDetails.productDetails, 
-    res,locals.email
+    res.locals.email
   ], (err, results) => {
     if(!err) {
-      ejs.renderFile(path.join(__dirname, '', "report.ejs", {
+      ejs.renderFile(path.join(__dirname, '', "report.ejs"), {
         productDetails: productDetailsReport, 
         name: orderDetails.name, 
         email: orderDetails.email, 
@@ -34,7 +34,7 @@ router.post('/generateReport', auth.authenticateToken, (req, res) => {
         totalAmount: orderDetails.totalAmount
       }, (err, results) => {
         if (!err) {
-          pdf.create(data).toFile('./generated_pdf/' + generatedUuid + '.pdf', (err, data) => {
+          pdf.create(results).toFile('./generated_pdf/' + generatedUuid + '.pdf', (err, data) => {
             if (err) {
               console.log(err)
               return res.status(500).json(err);
@@ -45,11 +45,45 @@ router.post('/generateReport', auth.authenticateToken, (req, res) => {
         } else {
           return res.status(500).json(err)
         }
-      }))
+      })
     } else {
       return res.status(500).json(err);
     }
   })
+})
+
+router.post('/getPdf', auth.authenticateToken, (req, res) => {
+  const orderDetails = req.body;
+  const pdfPath = './generated_pdf/' + orderDetails.uuid + '.pdf';
+
+  if (fs.existsSync(pdfPath)) {
+    res.contentType('application/pdf');
+    fs.createReadStream(pdfPath).pipe(res);
+  } else {
+    let productDetailsReport = JSON.parse(orderDetails.productDetails);
+    ejs.renderFile(path.join(__dirname, '', "report.ejs"), {
+      productDetails: productDetailsReport, 
+      name: orderDetails.name, 
+      email: orderDetails.email, 
+      contactNumber: orderDetails.contactNumber, 
+      paymentMethod: orderDetails.paymentMethod,
+      totalAmount: orderDetails.totalAmount
+    }, (err, results) => {
+      if (!err) {
+        pdf.create(results).toFile('./generated_pdf/' + orderDetails.uuid + '.pdf', (err, data) => {
+          if (err) {
+            console.log(err)
+            return res.status(500).json(err);
+          } else {
+            res.contentType('application/pdf');
+            fs.createReadStream(pdfPath).pipe(res);
+          }
+        })
+      } else {
+        return res.status(500).json(err)
+      }
+    })
+  }
 })
 
 module.exports = router;
