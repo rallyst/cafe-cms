d8020cb513530b857fef3d29d17af961b4c0a756
Roman <rallyshops@gmail.com>
2023-01-05 19:40:51 +0200

    added login

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -34,7 +34,7 @@ export class UserService {
 
   login(data: any) {
     return this.httpClient.post(
-      this.url + '/user/signup', 
+      this.url + '/user/login', 
       data,
       {
         headers: new HttpHeaders().set('Content-Type', 'application/json')
