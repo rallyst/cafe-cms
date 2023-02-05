2b347f7ee9aaba3c7ba63137c6e3460c44815a4c
Roman <rallyshops@gmail.com>
2023-02-05 13:20:48 +0200

    category handler

--- frontend/src/app/layouts/full/header/header.component.ts
+++ frontend/src/app/layouts/full/header/header.component.ts
@@ -11,7 +11,8 @@ import { ConfirmationComponent } from 'src/app/material-component/dialog/confirm
 })
 export class AppHeaderComponent {
   role: any;
-  constructor(private router: Router,
+  constructor(
+    private router: Router,
     private dialog: MatDialog) {
   }
 
--- frontend/src/app/material-component/manage-category/manage-category.component.ts
+++ frontend/src/app/material-component/manage-category/manage-category.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
 import { NgxUiLoaderService } from 'ngx-ui-loader';
 import { CategoryService } from 'src/app/services/category.service';
 import { SnackbarService } from 'src/app/services/snackbar.service';
 import { GlobalConstants } from 'src/app/shared/global-constants';
+import { CategoryComponent } from '../dialog/category/category.component';
 
 @Component({
   selector: 'app-manage-category',
@@ -17,6 +19,7 @@ export class ManageCategoryComponent implements OnInit {
   responseMessage: any; 
 
   constructor(
+    private dialog: MatDialog,
     private categoryService: CategoryService,
     private ngxService: NgxUiLoaderService,
     private snackbarService: SnackbarService,
@@ -49,7 +52,18 @@ export class ManageCategoryComponent implements OnInit {
   }
 
   handleAddAction() {
-
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.data = {
+      action: 'Add'
+    }
+    dialogConfig.width = '850px';
+    const dialogRef = this.dialog.open(CategoryComponent, dialogConfig);
+    this.router.events.subscribe(() => {
+      dialogRef.close();
+    });
+    const sub = dialogRef.componentInstance.onAddCategory.subscribe(response => {
+      this.tableData();
+    })
   }
 
   handleEditAction(value: any) {
